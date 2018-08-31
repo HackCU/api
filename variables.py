f612e5a5db88e7d526fad706822b9fc005c14182
Gerard Casas Saez <casassg@gmail.com>
2018-08-31 16:46:22 -0600

    enable cors

--- variables.py
+++ variables.py
@@ -2,3 +2,7 @@ import os
 SLACK = os.environ.get('SLACK', None)
 SENDGRID = os.environ.get('SENDGRID', None)
 DEBUG = os.environ.get('DEBUG', 'true') == 'true'
+
+# CORS config
+
+CORS_ORIGIN_REGEX_WHITELIST = (r'^(https?://)?(\w+\.)?hackcu\.org$', )
